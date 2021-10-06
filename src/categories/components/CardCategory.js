74251203a12575a517898c4f7eb379056f235b71
memoadian <memoadian@gmail.com>
2021-10-06 02:38:24 -0500

    editar categoría

--- src/categories/components/CardCategory.js
+++ src/categories/components/CardCategory.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
-const CardCategory = props => {
+const CardCategory = (props) => {
+  const [color, setColor] = useState(null);
 
-    const trigger = () => {
-        props.onClickEdit({editMode: true, id: props.id})
+  useEffect(() => {
+    switch (props.background) {
+      case "amarillo":
+        setColor("#fff654");
+        break;
+      case "azul":
+        setColor("#54d1ff");
+        break;
+      case "morado":
+        setColor("#c6a5e2");
+        break;
+      case "naranja":
+        setColor("#ffbb84");
+        break;
+      case "turqueza":
+        setColor("#67BAAD");
+        break;
+      case "verde":
+        setColor("#55CF45");
+        break;
     }
+    console.log(color);
+  }, [props.color]);
 
-    return (
-        <>
-            <div className="card-category">
-                <div className="card-category-header" style={{backgroundColor: `${props.background}`}}>
-                    {props.title}
-                </div>
-                <div className="card-category-body">
-                    {props.description}
-                </div>
-                <div className="card-category-footer">
-                    <NavLink to={`/category/${props.id}`}>
-                        <span className="left">Ingresar</span>
-                    </NavLink>
-                    <span 
-                        className="right"
-                        onClick={trigger} >
-                        Editar
-                    </span>
-                </div>
-            </div>
-        </>
-    )
-}
+  const trigger = () => {
+    props.onClickEdit({ editMode: true, id: props.id });
+  };
 
-export default CardCategory
+  return (
+    <>
+      <div className="card-category">
+        <div
+          className="card-category-header"
+          style={{ backgroundColor: `${color}` }}
+        >
+          {props.title}
+        </div>
+        <div className="card-category-body">{props.description}</div>
+        <div className="card-category-footer">
+          <NavLink to={`/category/${props.id}`}>
+            <span className="left">Ingresar</span>
+          </NavLink>
+          <span className="right" onClick={trigger}>
+            Editar
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardCategory;
--- src/categories/pages/Caps.js
+++ src/categories/pages/Caps.js
@@ -1,286 +1,291 @@
-import {useState, useContext, useEffect} from 'react'
-import { AuthContext } from 'shared/context/auth-context'
-import { useForm } from 'shared/hooks/form-hook'
-import axios from 'axios'
-import Scrollbars from 'react-custom-scrollbars-2'
-import Modal from 'react-modal'
-import CardCategory from 'categories/components/CardCategory'
-import Loader from 'shared/UIElements/Loader'
-import Input from 'shared/components/FormElements/Input'
-import ImageUpload from 'shared/components/FormElements/ImageUpload'
+import { useState, useContext, useEffect } from "react";
+import { AuthContext } from "shared/context/auth-context";
+import { useForm } from "shared/hooks/form-hook";
+import axios from "axios";
+import Scrollbars from "react-custom-scrollbars-2";
+import Modal from "react-modal";
+import CardCategory from "categories/components/CardCategory";
+import Loader from "shared/UIElements/Loader";
+import Input from "shared/components/FormElements/Input";
+import ImageUpload from "shared/components/FormElements/ImageUpload";
 
 const Caps = () => {
-    const auth = useContext(AuthContext)
-    const [isLoading, setIsLoading] = useState(false)
-    const [openModal, setOpenModal] = useState(false)
-    const [categories, setCategories] = useState([])
-    const [isEditMode, setIsEditMode] = useState(false)
-    const [categorySelected, setCategorySelected] = useState(null)
-    const [color, setColor] = useState(null)
-    const [formState, inputHandler] = useForm({
-        title: {
-            value: '',
-            isValid: false
+  const auth = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [categorySelected, setCategorySelected] = useState(null);
+  const [color, setColor] = useState(null);
+  const [colors, setColors] = useState([]);
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      color: {
+        value: "",
+        isValid: false,
+      },
+      image: {
+        value: null,
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const fetchCategories = async () => {
+    setIsLoading(true);
+    const response = await axios({
+      headers: {
+        Authorization: `Bearer ${auth.token}`,
+      },
+      baseURL: "https://api.wumi.app/api/v1/catalog/categories/?type_content=1",
+      method: "GET",
+    });
+
+    setCategories(response.data.results);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (!auth.token) {
+      return;
+    }
+    fetchCategories();
+    setColors([
+      { id: "azul", title: "Azul" },
+      { id: "amarillo", title: "Amarillo" },
+      { id: "verde", title: "Verde" },
+      { id: "naranja", title: "Naranja" },
+      { id: "turqueza", title: "Turqueza" },
+      { id: "morado", title: "Morado" },
+    ]);
+  }, [auth]);
+
+  const handleOpenModal = (data) => {
+    if (data.editMode) {
+      setIsEditMode(true);
+    }
+    if (data.id !== null) {
+      setCategorySelected(categories.find((cat) => cat.id === data.id));
+    }
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsEditMode(false);
+    setOpenModal(false);
+  };
+
+  const submitCategory = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", formState.inputs.title.value);
+    formData.append("description", formState.inputs.description.value);
+    formData.append("color", formState.inputs.color.value);
+    formData.append("type_content_id", 1);
+    formData.append("is_active", 1);
+    formData.append("image", formState.inputs.image.value);
+
+    try {
+      setIsLoading(true);
+      const resp = await axios({
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
         },
-        description: {
-            value: '',
-            isValid: false
+        baseURL: `https://api.wumi.app/api/v1/catalog/categories/`,
+        method: "POST",
+        mode: "no-cors",
+        data: formData,
+      });
+
+      setIsLoading(false);
+
+      if (resp.status === 201) {
+        handleCloseModal();
+        fetchCategories();
+      } else {
+        //setError(resp)
+        console.log(resp.status);
+      }
+    } catch (err) {
+      setIsLoading(false);
+      //setError(err.errors || 'Something went wrong, please try again.')
+    }
+  };
+
+  const updateCategory = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", formState.inputs.title.value);
+    formData.append("description", formState.inputs.description.value);
+    formData.append("color", formState.inputs.color.value);
+    formData.append("is_active", 1);
+
+    try {
+      setIsLoading(true);
+      const resp = await axios({
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
         },
-        color: {
-            value: '',
-            isValid: false
-        },
-        image: {
-            value: null,
-            isValid: false
-        },
-    }, false)
+        baseURL: `https://api.wumi.app/api/v1/catalog/categories/${categorySelected.id}/`,
+        method: "PATCH",
+        mode: "no-cors",
+        data: formData,
+      });
 
-    const fetchCategories = async () => {
-        setIsLoading(true)
-        const response = await axios({
-            headers: {
-                Authorization: `Bearer ${auth.token}`
-            },
-            baseURL: 'https://api.wumi.app/api/v1/catalog/categories/?type_content=1',
-            method: 'GET',
-        })
+      setIsLoading(false);
 
-        setCategories(response.data.results)
-        setIsLoading(false)
+      if (resp.status === 200) {
+        handleCloseModal();
+        fetchCategories();
+      } else {
+        //setError(resp)
+        console.log(resp.status);
+      }
+    } catch (err) {
+      setIsLoading(false);
+      //setError(err.errors || 'Something went wrong, please try again.')
     }
+  };
 
-    useEffect(() => {
-        if (!auth.token) {return}
-        fetchCategories()
-    }, [auth])
+  const customStyles = {
+    content: {
+      width: "50%",
+      height: "80%",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      padding: "36px 75px",
+      border: "none",
+      borderRadius: "30px",
+    },
+  };
 
-    const handleOpenModal = (data) => {
-        if (data.editMode) {
-            setIsEditMode(true)
-        }
-        if (data.id !== null) {
-            setCategorySelected(categories.find(cat => cat.id === data.id))
-        }
-        setOpenModal(true)
-    }
+  const listItems = categories.map(({ id, title, color, description }) => (
+    <div key={id} className="column is-one-quarter-desktop is-half-tablet">
+      <CardCategory
+        id={id}
+        title={title}
+        background={color}
+        description={description}
+        onClickEdit={handleOpenModal}
+      />
+    </div>
+  ));
 
-    const handleCloseModal = () => {
-        setIsEditMode(false)
-        setOpenModal(false)
-    }
-
-    const submitCategory = async e => {
-        e.preventDefault()
-
-        const formData = new FormData()
-        formData.append('title', formState.inputs.title.value)
-        formData.append('description', formState.inputs.description.value)
-        formData.append('color', formState.inputs.color.value)
-        formData.append('type_content_id', 1)
-        formData.append('is_active', 1)
-        formData.append('image', formState.inputs.image.value)
-
-        try {
-            setIsLoading(true)
-            const resp = await axios({
-                headers: {
-                    Authorization: `Bearer ${auth.token}`
-                },
-                baseURL: `https://api.wumi.app/api/v1/catalog/categories/`,
-                method: 'POST',
-                mode: 'no-cors',
-                data: formData
-            })
-
-            setIsLoading(false)
-                
-            if (resp.status === 201) {
-                handleCloseModal()
-                fetchCategories()
-            } else {
-                //setError(resp)
-                console.log(resp.status)
-            }
-        } catch (err) {
-            setIsLoading(false)
-            //setError(err.errors || 'Something went wrong, please try again.')
-        }
-    }
-
-    const updateCategory = async e => {
-        e.preventDefault()
-
-        const formData = new FormData()
-        formData.append('title', formState.inputs.title.value)
-        formData.append('description', formState.inputs.description.value)
-        formData.append('color', formState.inputs.color.value)
-        formData.append('is_active', 1)
-
-        try {
-            setIsLoading(true)
-            const resp = await axios({
-                headers: {
-                    Authorization: `Bearer ${auth.token}`
-                },
-                baseURL: `https://api.wumi.app/api/v1/catalog/categories/${categorySelected.id}/`,
-                method: 'PATCH',
-                mode: 'no-cors',
-                data: formData
-            })
-
-            setIsLoading(false)
-                
-            if (resp.status === 200) {
-                handleCloseModal()
-                fetchCategories()
-            } else {
-                //setError(resp)
-                console.log(resp.status)
-            }
-        } catch (err) {
-            setIsLoading(false)
-            //setError(err.errors || 'Something went wrong, please try again.')
-        }
-    }
-
-    const customStyles = {
-        content: {
-            width: '50%',
-            height: '80%',
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            padding: '36px 75px',
-            border: 'none',
-            borderRadius: '30px'
-        },
-    }
-
-    const listItems = categories.map(({id, title, color, description}) => 
-        <div
-            key={id}
-            className="column is-one-quarter-desktop is-half-tablet">
-            <CardCategory 
-                id={id} 
-                title={title}
-                background={color}
-                description={description}
-                onClickEdit={handleOpenModal}
-            />
-        </div>
-    )
-
-    return (
-        <div>
-            <div className="title-h1">
-                <h1>Categorías Capsulas</h1>
-                <button 
-                    className="button right-h1" 
-                    onClick={handleOpenModal}>
-                    Nueva Categoría
-                </button>
-            </div>
-            <div className="card no-margin">
-                {isLoading && <Loader asOverlay />}
-                <Scrollbars
-                    style={{ height: 650}}
-                    renderTrackHorizontal={props => 
-                        <div {...props} style={{display: 'none'}} />
-                    }
+  return (
+    <div>
+      <div className="title-h1">
+        <h1>Categorías Capsulas</h1>
+        <button className="button right-h1" onClick={handleOpenModal}>
+          Nueva Categoría
+        </button>
+      </div>
+      <div className="card no-margin">
+        {isLoading && <Loader asOverlay />}
+        <Scrollbars
+          style={{ height: 650 }}
+          renderTrackHorizontal={(props) => (
+            <div {...props} style={{ display: "none" }} />
+          )}
+        >
+          <div className="padding-container" style={{ padding: "20px" }}>
+            <div className="columns is-multiline">{listItems}</div>
+          </div>
+        </Scrollbars>
+        <Modal
+          ariaHideApp={false}
+          isOpen={openModal}
+          style={customStyles}
+          onRequestClose={handleCloseModal}
+          overlayClassName="Overlay"
+        >
+          <h1>{isEditMode ? "Editar Categoría" : "Nueva Categoría"}</h1>
+          <form className="form-modal">
+            <div className="columns">
+              <div className="column">
+                <Input
+                  id="title"
+                  value={isEditMode ? categorySelected.title : ""}
+                  element="text"
+                  label="Nombre"
+                  validators={[]}
+                  onInput={inputHandler}
+                />
+                <Input
+                  id="description"
+                  value={isEditMode ? categorySelected.description : ""}
+                  element="textarea"
+                  label="Descripción"
+                  validators={[]}
+                  onInput={inputHandler}
+                />
+                <div className="columns">
+                  <div className="column">
+                    <Input
+                      id="color"
+                      label="Nivel"
+                      element="select"
+                      validators={[]}
+                      onInput={inputHandler}
                     >
-                    <div className="padding-container" style={{padding: '20px'}}>
-                        <div className="columns is-multiline">
-                            {listItems}
-                        </div>
-                    </div>
-                </Scrollbars>
-                <Modal
-                    ariaHideApp={false}
-                    isOpen={openModal}
-                    style={customStyles}
-                    onRequestClose={handleCloseModal}
-                    overlayClassName="Overlay">
-                    <h1>{isEditMode 
-                        ? 'Editar Categoría' 
-                        : 'Nueva Categoría'}</h1>
-                    <form className="form-modal">
-                        <div className="columns">
-                            <div className="column">
-                                <Input 
-                                    id="title"
-                                    value={isEditMode ? categorySelected.title : ''}
-                                    element="text"
-                                    label="Nombre"
-                                    validators={[]}
-                                    onInput={inputHandler}
-                                />
-                                <Input 
-                                    id="description"
-                                    value={isEditMode ? categorySelected.description : ''}
-                                    element="textarea"
-                                    label="Descripción"
-                                    validators={[]}
-                                    onInput={inputHandler}
-                                />
-                                <div className="columns">
-                                    <div className="column">
-                                        <Input 
-                                            id="color"
-                                            type="text"
-                                            value={isEditMode ? categorySelected.color : color}
-                                            element="text"
-                                            label="Color (Hexadecimal)"
-                                            validators={[]}
-                                            onInput={inputHandler}
-                                        />
-                                    </div>
-                                    <div className="column">
-                                        <Input 
-                                            id="color"
-                                            type="color"
-                                            value={isEditMode ? categorySelected.color : color}
-                                            onChange={e => setColor(e.target.value)}
-                                            element="text"
-                                            label="Color (Hexadecimal)"
-                                            validators={[]}
-                                            onInput={inputHandler}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="column">
-                                <ImageUpload 
-                                    center
-                                    id="image"
-                                    onInput={inputHandler}
-                                    errorText="Selecciona una imagen"
-                                />
-                            </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column buttons">
-                                <button
-                                    onClick={handleCloseModal}
-                                    type="button"
-                                    className="button cancel">
-                                    Cancelar
-                                </button>
-                                <button
-                                    onClick={isEditMode ? updateCategory : submitCategory}
-                                    className="button submit">
-                                    Guardar
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </Modal>
+                      <option value="">Seleccionar</option>
+                      {colors &&
+                        colors.map((color) => {
+                          return (
+                            <option key={color.id} value={color.id}>
+                              {color.title}
+                            </option>
+                          );
+                        })}
+                    </Input>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <ImageUpload
+                  center
+                  id="image"
+                  onInput={inputHandler}
+                  errorText="Selecciona una imagen"
+                />
+              </div>
             </div>
-        </div>
-    )
-}
+            <div className="columns">
+              <div className="column buttons">
+                <button
+                  onClick={handleCloseModal}
+                  type="button"
+                  className="button cancel"
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={isEditMode ? updateCategory : submitCategory}
+                  className="button submit"
+                >
+                  Guardar
+                </button>
+              </div>
+            </div>
+          </form>
+        </Modal>
+      </div>
+    </div>
+  );
+};
 
-export default Caps
+export default Caps;
